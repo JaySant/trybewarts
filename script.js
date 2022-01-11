89a2e5e680879b47fb54a54b9a5b26f8b5f12cfe
RamosFonseca <beidseitig@gmail.com>
2022-01-11 16:17:31 -0300

    Requisito 3, validando o email e o login com javascript

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 let submit = document.querySelector("#submit");
+// let email = emailOriginal.toString();
+// let password = passwordOrinal.toString();
 
 function login (event) {
     event.preventDefault();
 
     if (email.value === "tryber@teste.com" && password.value === "123456" ) {
-        alert = "Olá, Tryber!";
+        alert ("Olá, Tryber!");
     } else {
-        alert = "Email ou senha inválidos.";
+        alert ("Email ou senha inválidos.");
     }
 }
 
-submit.addEventListener('clicl', login);
+submit.addEventListener('click', login);
