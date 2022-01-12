9cc5eb11a76a582ec29a74ab85bd5b0714beebfb
RamosFonseca <beidseitig@gmail.com>
2022-01-12 15:18:16 -0300

    Ajustando o codigo do javascript pro lint

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const submit = document.querySelector('#submit');
 function login(event) {
   event.preventDefault();
 
-  if (email.value === "tryber@teste.com" && password.value === "123456" ) {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
